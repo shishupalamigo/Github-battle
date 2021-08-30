bb1c1bcb0a167358e69e55ed28c5c7f9f125c8c9
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-08-30 21:48:14 +0530

    Add GitHub Battle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route,  } from "react-router-dom";
+import Header from "./components/Header";
+import GitHubStars from "./components/GitHubStars";
+import GithubBattle from "./components/GitHubBattle";
+import Battle from "./components/Battle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkMode: false
+    }
+  }
+  toggleDarkMode = () => {
+    this.setState((prevState) => ({ darkMode: !prevState.darkMode }));
+  } 
+
+  render(){
+    return (
+      <div className={this.state.darkMode ? "bg-gray-900 text-white min-h-screen": ""}>
+      <BrowserRouter>
+      <Header toggleDarkMode={this.toggleDarkMode} {...this.state }/>
+      <Route path="/" exact>
+        <GitHubStars  {...this.state}/>
+      </Route>
+      <Route path="/battle" exact>
+        <GithubBattle  {...this.state}/>
+      </Route>
+      <Route path="/userbattle" component={Battle} {...this.state}></Route>
+      </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
